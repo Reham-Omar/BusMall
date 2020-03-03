7faa48f196b27d64d7a9ebd56b0433f29642842a
Reham-omar <rehamalsobh96@gmail.com>
2020-03-03 22:11:07 +0200

    done

--- js/apps.js
+++ js/apps.js
@@ -89,9 +89,7 @@ function clickImage(event) {
     listResult();
     chartResult();
 
-    // ctx.setAttribute( 'width','300px');
-    // ctx.setAttribute( 'height','300px');
-    
+       
   }
 }
 allpro.addEventListener('click', clickImage);
@@ -175,55 +173,6 @@ function chartResult() {
   }
   
   );
-
-
-  // ctx.setAttribute( 'width','300px');
-  // ctx.setAttribute( 'height','300px');
-  
 }
 // getItem();
 
-// ctx.setAttribute( 'width','300px');
-// ctx.setAttribute( 'height','300px');
-
-// var ctx = document.getElementById('myObject').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: productName,
-//         datasets: [{
-//             label: '# of Votes',
-//             data:clickobject ,
-           
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             label: '# of viewed',
-//             data:viewsArr ,
-//             bclicksColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             bclicksWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
-// }
