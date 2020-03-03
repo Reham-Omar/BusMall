924841815d33c68b9f5d70cd081e974a80d0f807
Reham-omar <rehamalsobh96@gmail.com>
2020-03-03 07:09:03 +0200

    gggg

--- js/apps.js
+++ js/apps.js
@@ -14,26 +14,30 @@ var centerImage = document.querySelector('#center_img');
 var rightImage = document.querySelector('#right_img');
 var busmallobjects = [];//an array to store all busmallobjects object
 var totalClicks = 0;
-var groupImageSection = document.querySelector('#all_pic')
-
+var allpro = document.querySelector('#allproduct');
+var leftImageRandom ,centerImageRandom ,rightImageRandom;
 function Busmall(name) {
   this.name = name;
   this.urlImage = `images/${this.name}`;
   busmallobjects.push(this);
+  this.view = 0;
+  this.clickTime = 0
 }
-for (var i = 0; i < busmallobjectsImages.length; i++) {
-  new Busmall(busmallobjectsImages[i]);//we pass the name of the busmall from the array
-}
-groupImageSection.addEventListener('click', clickImage);
-pickRandomImages();
-clickImage();
+
 
 function pickRandomImages() {
-  var leftImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
-  var centerImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
-  var rightImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+  leftImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+  centerImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+  rightImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
 
-  
+
+  while (leftImageRandom === centerImageRandom || rightImageRandom === centerImageRandom || leftImageRandom === rightImageRandom) {
+    leftImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+    centerImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+    rightImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
+
+  }
+
   leftImage.setAttribute('src', leftImageRandom.urlImage);
   leftImage.setAttribute('alt', leftImageRandom.name);
 
@@ -43,35 +47,44 @@ function pickRandomImages() {
   rightImage.setAttribute('src', rightImageRandom.urlImage);
   rightImage.setAttribute('alt', rightImageRandom.name);
 
-  while (leftImage === centerImage ||  rightImage === centerImage ||leftImage===rightImage ) {
-  leftImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
-   centerImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
-   rightImageRandom = busmallobjects[randomNumber(0, busmallobjects.length - 1)];
-
-  
-  leftImage.setAttribute('src', leftImageRandom.urlImage);
-  leftImage.setAttribute('alt', leftImageRandom.name);
-    //pick another random 
-    centerImage.setAttribute('src', centerImageRandom.urlImage);
-  centerImage.setAttribute('alt', centerImageRandom.name);
-
-  rightImage.setAttribute('src', rightImageRandom.urlImage);
-  rightImage.setAttribute('alt', rightImageRandom.name);
-    
-  }
-  
-
 }
+for (var i = 0; i < busmallobjectsImages.length; i++) {
+  new Busmall(busmallobjectsImages[i]);//we pass the name of the busmall from the array
+}
+pickRandomImages();
+
 function clickImage(event) {
   if (event.target.id === 'left_img' || event.target.id === 'center_img' || event.target.id === 'right_img') {
     pickRandomImages();
     totalClicks++;
+    leftImageRandom.view++ ;
+    centerImageRandom.view++ ;
+    rightImageRandom.view++ ;
   }
-  console.log();
+  if (event.target.id ==="left_img") {
+    leftImageRandom.clickTime++ ;
+   }
+  
+   if (event.target.id ==="center_img") {
+    centerImageRandom.clickTime ++ ;
+    }
+    if (event.target.id ==="right_img") {
+      rightImageRandom.clickTime++ ;
+    }
   if (totalClicks === 25) {
     //remove event listener
-    Busmall.removeEventListener('click', clickImage);
+    allpro.removeEventListener('click', clickImage);
+    listResult();
+
   }
 }
+allpro.addEventListener('click', clickImage);
 
+function listResult(){
 
+  var ulEl = document.getElementById('results');
+  for (var i=0; i<busmallobjects.length;i++){
+  var liEl = document.createElement('li');
+  ulEl.appendChild(liEl);
+  liEl.textContent = `${busmallobjects[i].name } had ${busmallobjects[i].clickTime }  click  ${busmallobjects[i].view} view`;
+}}
